1f884ec36744b24c2e449a9d9c8e35e2f5678cc6
Eduardo Afán <eduardoafan.eae@gmail.com>
2024-11-01 18:36:09 +0100

    avance

---
+++ js/Script_precios.js
@@ -0,0 +1,27 @@
+
+    // Obtener el modal
+    var modal = document.getElementById("infoModal");
+
+    // Obtener el botón que abre el modal
+    var btn = document.getElementById("openModal");
+
+    // Obtener el elemento <span> que cierra el modal
+    var span = document.getElementById("closeModal");
+
+    // Cuando el usuario hace clic en el botón, abrir el modal 
+    btn.onclick = function() {
+        modal.style.display = "block";
+    }
+
+    // Cuando el usuario hace clic en <span> (x), cerrar el modal
+    span.onclick = function() {
+        modal.style.display = "none";
+    }
+
+    // Cuando el usuario hace clic en cualquier parte fuera del modal, cerrarlo
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+
